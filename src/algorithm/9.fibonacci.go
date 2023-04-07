3b88aada39d1e71d4124c9e95546571e8a53e5ea
arno <ykq12313@gmail.com>
2023-04-07 17:05:40 +0800

    fibonacci

--- src/algorithm/9.fibonacci.go
+++ src/algorithm/9.fibonacci.go
@@ -1,7 +1,12 @@
 package main
 
+// 递归三要素
+// 1. 定义：函数要接受什么样的参数  返回什么样的值，代表什么意思
+// 2. 递归的拆解： 通过处理把参数规模变小
+// 3. 递归的出口： 适时结束递归
+
 func main() {
-	print(fib(4))
+	print(fib(5))
 }
 
 func fib(n int) int {
