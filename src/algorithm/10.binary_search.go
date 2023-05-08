c3ed42fddeb9d910db631dd95889da59a5a82a93
arno <ykq12313@gmail.com>
2023-05-08 17:57:20 +0800

    binaryModel

--- src/algorithm/10.binary_search.go
+++ src/algorithm/10.binary_search.go
@@ -1,14 +1,15 @@
 package main
 
 func main() {
-	arr := []int{1, 2, 8, 9, 10, 12}
-	println(binarySearchRecursion(arr, 0, len(arr)-1, 1))
-	println(binarySearchNormal(arr, 12))
+	arr := []int{1, 2, 8, 9, 10, 10, 12}
+	//println(binarySearchRecursion(arr, 0, len(arr)-1, 1))
+	//println(binarySearchNormal(arr, 12))
+	println(binaryModel(arr, 9))
 }
 
 // 递归解法  o(logN)  是一种减治的思想，而不是分治
 func binarySearchRecursion(arr []int, start int, end int, target int) int {
-	// 跳出条件
+	// 跳出条件 一定要注意 否则死循环
 	if start > end {
 		return -1
 	}
@@ -47,4 +48,33 @@ func binarySearchNormal(arr []int, target int) int {
 	}
 
 	return -1
+}
+
+// 二分法模板 todo 重点记忆
+func binaryModel(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	start, end := 0, len(nums)-1
+
+	for start+1 < end { // 双指针相邻时就退出
+		mid := (start + end) / 2
+
+		if nums[mid] < target {
+			start = mid
+		} else if nums[mid] == target {
+			end = mid
+		} else {
+			end = mid
+		}
+	}
+
+	if nums[start] == target {
+		return start
+	}
+	if nums[end] == target {
+		return end
+	}
+	return 1
 }
